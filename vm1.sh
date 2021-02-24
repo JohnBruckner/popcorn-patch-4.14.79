3db6c28a2c2cbeded101abd37aa73c6f74edda74
Ion Bruckner <mihai.bruckner@gmail.com>
2021-02-24 05:14:52 +0000

    added popcorn specific imports

--- vm1.sh
+++ vm1.sh
@@ -2,7 +2,7 @@ sudo qemu-system-x86_64 -m 2048 -smp 4 \
     -hda $HOME/popcorn-vm/x86.img \
     -netdev tap,id=x86nic,ifname=tap0 -device e1000,netdev=x86nic,mac=52:54:00:12:34:60 \
     -nographic \
-    -kernel obj/linux-x86-basic/arch/x86_64/boot/bzImage \
+    -kernel obj/linux-x86-4.14-patch/arch/x86_64/boot/bzImage \
     -append "root=/dev/sda1 console=ttyS0"
 
 
