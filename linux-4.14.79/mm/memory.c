8f66c87cf537c422bb73b742660d6155e605b0bb
Ion Bruckner <mihai.bruckner@gmail.com>
2021-03-02 00:08:36 +0000

    fixed bug caused by improper merge earlier

--- linux-4.14.79/mm/memory.c
+++ linux-4.14.79/mm/memory.c
@@ -4053,23 +4053,23 @@ int handle_pte_fault_origin(struct mm_struct *mm,
 	spinlock_t *ptl;
 	pte_t entry = *pte;
 
-	struct mm_struct *mm = vma->vm_mm;
-	struct mem_cgroup *memcg;
+//	struct mm_struct *mm = vma->vm_mm;
+//	struct mem_cgroup *memcg;
 
 	struct vm_fault vmf = {
 		.vma = vma, // func arg
 		.address = address & PAGE_MASK, // func arg
 		.flags = flags, // func arg
-      .pmd = pmd
-      .pte = entry
-      .orig_pte = pte
+        .pmd = pmd,
+        .pte = &entry,
+        .orig_pte = pte,
 		.pgoff = linear_page_index(vma, address),
 		.gfp_mask = __get_fault_gfp_mask(vma),
 	};
 	barrier();
 
 	if (!vma_is_anonymous(vma))
-	    return do_fault(vmf);
+	    return do_fault(&vmf);
 //		return do_fault(mm, vma, address, pte, pmd, flags, entry);
 
 	/**
