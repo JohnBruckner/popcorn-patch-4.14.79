21bbb2b836c9c22efcc0a106cf42e2bc8cf8f507
Ion Bruckner <mihai.bruckner@gmail.com>
2021-03-02 21:07:43 +0000

    modified vm for debugging

--- vm1.sh
+++ vm1.sh
@@ -1,9 +1,6 @@
-sudo qemu-system-x86_64 -m 2048 -smp 4 \
+sudo qemu-system-x86_64 -m 2048 -smp 1 \
     -hda $HOME/popcorn-vm/x86.img \
     -netdev tap,id=x86nic,ifname=tap0 -device e1000,netdev=x86nic,mac=52:54:00:12:34:60 \
     -nographic \
     -kernel obj/linux-x86-4.14-patch/arch/x86_64/boot/bzImage \
-    -append "root=/dev/sda1 console=ttyS0"
-
-
-
+    -append "root=/dev/sda1 console=ttyS0" -s -S
